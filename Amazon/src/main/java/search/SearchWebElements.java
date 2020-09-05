4384571d4b22e6769a07fe31482051a6a197165c
waqaschaddhar <waqaschaudhry227@gmail.com>
2020-09-04 23:40:33 -0400

    test run with shakirs amazon

--- Amazon/src/main/java/search/SearchWebElements.java
+++ Amazon/src/main/java/search/SearchWebElements.java
@@ -1,12 +1,24 @@
 package search;
 
-public class SearchWebElements {
+import base.CommonAPI;
+import com.github.javafaker.Faker;
 
+public class SearchWebElements extends CommonAPI {
+
+    static Faker faker = new Faker();
+
+    public static final String randomBookSearch = faker.book().title();
+    public static final String expectedRandomBookSearchURL = "https://www.amazon.com/s?k=" + randomBookSearch.replace(" ", "+") + "&i=stripbooks-intl-ship&ref=nb_sb_noss";
+    public static final String expectedRandomBookSearchTitle = "Amazon.com : " + randomBookSearch;
     public static final String searchFieldIDWebElement = "twotabsearchtextbox";
-    public static final String searchButtonXPATHWebElement = "//span[@id='nav-search-submit-text']/input";
-    public static final String searchVerificationPartialTextWebElement = "\"iphone\"";
-    public static final String searchItem1 = "iphone";
+    public static final String searchButtonXPathWebElement = "//*[@id=\"nav-search-submit-text\"]/input";
+    public static final String searchTextXpathWebElement = "//span[@class='a-color-state a-text-bold']\"";
+    public static final String searchVerificationTextXPathWebElement = "//span[text()='\"Hand Sanitizer\"']";
+    public static final String allDropDownSearchIDWebElement = "searchDropdownBox";
+    public static final String booksOnAllDropDownXpathWebElement = "//*[@id=\"searchDropdownBox\"]/option[6]";
+    public static final String brandCheckBoxPurellXPathWebElement = "//span[text()='Purell']";
+    public static final String purellTextXPathWebElement = "//*[@id=\"search\"]/span/div/span/h1/div/div[1]/div/div/a/span";
+    public static final String moviesAndTVOnAllDropDownXpathWebElement = "//span[text()='Movies & TV']";
 
-    public static final String  findAndReserveXPathWebElement = "//*[@id=\"MainNavLinkde94\"]/a";
 
 }
--- Amazon/src/test/java/search/SearchTest.java
+++ Amazon/src/test/java/search/SearchTest.java
@@ -4,6 +4,7 @@ import base.CommonAPI;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import reporting.TestLogger;
 
 public class SearchTest extends CommonAPI {
 
@@ -14,9 +15,93 @@ public class SearchTest extends CommonAPI {
         search = PageFactory.initElements(driver, Search.class);
     }
 
+    /**
+     * This test searches a single item, 'Hand sanitizer' and verifies it with "Hand sanitizer" text on the page.
+     */
+    @Test
+    public void testSearchSingleItem() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        search.searchHandSanitizer();
+        search.validateSearchHandSanitizerText();
+    }
+
+//    /**
+//     * This test searches multiple items.
+//     */
 //    @Test
-//    public void testSearchIphone() {
-//        search.searchIphone();
-//        search.validateIphoneSearch();
+//    public void testSearchMultipleItems() {
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        search.clearTypeAndClickOnSearch();
+//        search.validateSearchMultipleItems();
 //    }
+//
+//    /**
+//     * This data-driven tests searches items stored on MySQL.
+//     *
+//     * @throws Exception
+//     */
+//    @Test(enabled = false)
+//    public void testValidateSearchItemsFromMySQL() throws Exception {
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//       // search.validateSearchItemsFromMySQL();
+//    }
+//
+//    /**
+//     * This test validates all the items on 'All' dropdown on search.
+//     */
+//    @Test
+//    public void testValidateAllDropDown() {
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        search.validateAllDropDown();
+//    }
+//
+//    /**
+//     * This test verifies that the text 'Purell' is be displayed
+//     * after searching for "Hand sanitizer", then,
+//     * click on the 'Purell' option on the 'Brand' checkbox.
+//     * Finally, validate with 'Purell' checkbox in 'brand' selection being enabled.
+//     */
+//    @Test
+//    public void testVerifyPurellTextIsDisplayed() {
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        search.searchByBrandPurellHandSanitizer();
+//        search.verifyPurellTextIsDisplayed();
+//    }
+//
+//    /**
+//     * This test searches random book and validates it by URL.
+//     */
+//    @Test
+//    public void testVerifySearchRandomBooksByURL() {
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        search.searchRandomBook();
+//        search.verifySearchRandomBookByURL();
+//    }
+//
+//    /**
+//     * This test searches random book and validates it by title.
+//     */
+//    @Test
+//    public void testVerifySearchRandomBooksByTitle() {
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        search.searchRandomBook();
+//        search.verifySearchRandomBookByTitle();
+//    }
+//
+////    @Test
+////    public void testSearchBoxCheckGetItemsListFromExcel() throws Exception {
+////            TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+////    }.getClass().getEnclosingMethod().getName()));
+////        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+////        }.getClass().getEnclosingMethod().getName()));
+////        search.searchBoxCheckGetItemsListFromExcel();
+////    }
+
 }
